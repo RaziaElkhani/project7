6af05fb709a6ab50dca78a5e0d30f15d80810232
RaziaElkhani <elkhanirazia@gmail.com>
2025-01-30 20:58:48 -0800

    Update TestDemo.java

--- src/demo2/TestDemo.java
+++ src/demo2/TestDemo.java
@@ -3,6 +3,9 @@ package demo2;
 public class TestDemo {
 public static void main(String[] args) {
 	add(12, 20);
+	add(20,20);
+	add(5,6);
+	add(30,30);
 }
 public static void add(int a,int b) {
 	System.out.println(a+b);
